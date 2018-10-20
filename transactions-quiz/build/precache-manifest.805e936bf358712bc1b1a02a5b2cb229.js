d60ffa385bcf6574b8340c62a55389206952dff2
ivan <acccko@gmail.com>
2018-10-20 19:43:00 +0100

    static content

---
+++ transactions-quiz/build/precache-manifest.805e936bf358712bc1b1a02a5b2cb229.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "09ab43336a4fcc31c713ecc47f3a3aa6",
+    "url": "/static/media/currency3.09ab4333.png"
+  },
+  {
+    "revision": "53783b9b385c2436d286",
+    "url": "/static/css/main.2b64b7fd.chunk.css"
+  },
+  {
+    "revision": "7c317d72271a7ce9daff",
+    "url": "/static/js/1.7c317d72.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d8ab2ab3571eaee92a6e9d405c5c419",
+    "url": "/static/media/currency1.3d8ab2ab.png"
+  },
+  {
+    "revision": "b3e82f86390177036dabd74d7b129bd8",
+    "url": "/static/media/currency2.b3e82f86.png"
+  },
+  {
+    "revision": "53783b9b385c2436d286",
+    "url": "/static/js/main.53783b9b.chunk.js"
+  },
+  {
+    "revision": "a6083cb5e6ec6da9b93266fb8e8240e6",
+    "url": "/static/media/currency4.a6083cb5.png"
+  },
+  {
+    "revision": "490845278321b89a0680af2a7d4ec2b7",
+    "url": "/static/media/fox.49084527.png"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/static/media/logo.84287d09.svg"
+  },
+  {
+    "revision": "2f28fc97e35fe0f8c863b1c261d6890c",
+    "url": "/index.html"
+  }
+];
